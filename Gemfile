1af440d449be548b0cd8e2c64589da32f05ddd52
yesukhe <eaxionn@gmail.com>
2023-12-23 21:41:00 +0900

    Add Solidus

--- Gemfile
+++ Gemfile
@@ -65,3 +65,37 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+gem "figaro"
+
+gem 'solidus', github: 'solidusio/solidus'
+
+gem "solidus_admin", "~> 0.1.0"
+
+gem "solidus_auth_devise", "~> 2.5"
+gem "responders"
+gem "canonical-rails"
+gem "solidus_support"
+gem "truncate_html"
+gem "view_component", "~> 3.0"
+gem "tailwindcss-rails"
+
+group :test do
+  gem "capybara-screenshot", "~> 1.0"
+  gem "database_cleaner", "~> 2.0"
+end
+
+group :development, :test do
+  gem "rspec-rails"
+  gem "rails-controller-testing", "~> 1.0.5"
+  gem "rspec-activemodel-mocks", "~> 1.1.0"
+  gem "factory_bot", ">= 4.8"
+  gem "factory_bot_rails"
+  gem "ffaker", "~> 2.13"
+  gem "rubocop", "~> 1.0"
+  gem "rubocop-performance", "~> 1.5"
+  gem "rubocop-rails", "~> 2.3"
+  gem "rubocop-rspec", "~> 2.0"
+end
+
+gem "solidus_stripe", "~> 5.a"
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,28 @@ Bundler.require(*Rails.groups)
 
 module Tennisworld
   class Application < Rails::Application
+    if defined?(FactoryBotRails)
+      initializer after: "factory_bot.set_factory_paths" do
+        require 'spree/testing_support/factory_bot'
+
+        # The paths for Solidus' core factories.
+        solidus_paths = Spree::TestingSupport::FactoryBot.definition_file_paths
+
+        # Optional: Any factories you want to require from extensions.
+        extension_paths = [
+          # MySolidusExtension::Engine.root.join("lib/my_solidus_extension/testing_support/factories"),
+          # or individually:
+          # MySolidusExtension::Engine.root.join("lib/my_solidus_extension/testing_support/factories/resource.rb"),
+        ]
+
+        # Your application's own factories.
+        app_paths = [
+          Rails.root.join('spec/factories'),
+        ]
+
+        FactoryBot.definition_file_paths = solidus_paths + extension_paths + app_paths
+      end
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
